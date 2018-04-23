23b5179168b81899b9c561000447a3dadba64a69
Davilicus <florisslenderman@gmail.com>
2018-04-23 14:44:15 +0200

    Added /hello and deployment ready

--- gitex/server.js
+++ gitex/server.js
@@ -6,9 +6,16 @@ app.all('*', (req,res,next) => {
     next();
 });
 
+app.get('/hello', (req, res, next) => {
+    res.status(200).json({
+        'msg': 'Hello JEDI\'s'
+    });
+});
+
 //Start the server
-app.listen(8080, () => {
-    console.log('The magic happens at http://localhost:' + 8080);
+const port = 8080;
+app.listen(port, () => {
+    console.log('The magic happens at http://localhost:' + port);
 });
 
 module.exports = app;
